455524fb5a15b5e86a053114b89c56f58d5645ff
woo9578 <38120520+woo9578@users.noreply.github.com>
2018-04-11 21:04:59 +0900

    Update Math.c

--- Math.c
+++ Math.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #pragma warning (disable:4996)
+
 int Sumfunction(int a, int b);
+int Mul(int num1, int num2);
+double Div(double num1, double num2);
+int Min(int num1, int num2);
 
 int main()
 {
   int i,j;
-  int result;
+  int result, mul, div, min;
   
   printf("정수 두개를 입력하시오.\n");
-  scanf("%d %d",&i,&j);
+  scanf_s("%d %d",&i,&j);
   
   result = Sumfunction(i,j);
+  mul = Mul(i,j);
+  div= Div(i,j);
+  min= Min(i,j);
   
   printf("Sum =%d\n",result);
   printf("mul= %d\n",i*j);
@@ -24,3 +31,16 @@ int Sumfunction(int a, int b)
 {
   return a+b;
 }
+
+int Mul(int num1, int num2){
+  return num1*num2;
+}
+
+double Div(double num1, double num2){
+  return num1/num2;
+}
+
+int Min(int num1, int num2){
+  return num1-num2;
+}
+
